56fbec3ecc5800f94d862dd12f92b54cbba48791
Brayan Arevalo <74871076+comofueps@users.noreply.github.com>
2024-06-17 11:05:05 -0500

    Add files via upload

--- src/database.js
+++ src/database.js
@@ -1,14 +1,14 @@
 //const mongoose = require('mongoose')
 import mongoose from "mongoose";
 const uri_local = "mongodb://localhost:27017";
-const uri_brayan =
-  "mongodb+srv://bearevalo:V9FhzTlm0ylumTbo@cluster0.yobuluw.mongodb.net/vidaplena?retryWrites=true&w=majority";
-//const uri =("mongodb+srv://marioquintero:IpsMed1gr0up1115a@medigroup-cluster.2svt4dr.mongodb.net/vidaplena?retryWrites=true&w=majority");
+
+const uri =
+  "mongodb+srv://marioquintero:IpsMed1gr0up1115a@medigroup-cluster.2svt4dr.mongodb.net/preprod_vidaplena?retryWrites=true&w=majority";
 
 export const connectDB = async () => {
   try {
     //await mongoose.connect(uri_local + "/vidaplena_local");
-    await mongoose.connect(uri_brayan);
+    await mongoose.connect(uri);
     // await mongoose.connect(uri)
     console.log(">>> DB is connected");
   } catch (error) {
