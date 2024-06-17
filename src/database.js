35080a97dea6a8fe5cb378d20b940d84af849f40
Brayan Arevalo <74871076+comofueps@users.noreply.github.com>
2024-06-17 10:44:24 -0500

    Add files via upload

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 const uri_local = "mongodb://localhost:27017";
 const uri_brayan =
-  "mongodb+srv://bearevalo:V9FhzTlm0ylumTbo@cluster0.yobuluw.mongodb.net/vidaplena?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://bearevalo:V9FhzTlm0ylumTbo@cluster0.yobuluw.mongodb.net/vidaplena?retryWrites=true&w=majority";
 //const uri =("mongodb+srv://marioquintero:IpsMed1gr0up1115a@medigroup-cluster.2svt4dr.mongodb.net/vidaplena?retryWrites=true&w=majority");
 
 export const connectDB = async () => {
